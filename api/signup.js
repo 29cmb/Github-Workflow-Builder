a8aa3875d9f6eb9621e2028aa3f15f0f95c1e3e7
Cmb <71194682+29cmb@users.noreply.github.com>
2024-07-31 12:11:42 -0400

    Signup

--- api/signup.js
+++ api/signup.js
@@ -1,6 +1,25 @@
+const db = require("../modules/db.js");
+const { encrypt } = require("../modules/encrypt.js");
 module.exports = (app) => {
-    app.post("/signup", (req, res) => {
+    app.post("/signup", async (req, res) => {
+        const { body } = req
+        if(
+            body == undefined 
+            || body.username == undefined 
+            || body.password == undefined
+            || typeof body.username != "string"
+            || typeof body.password != "string"
+        ) return res.status(400).json({ success: false, message: "Username or password not provided or not formatted properly" });
 
+        await db.client.connect()
+        const user = await db.collections.credentials.findOne({ username: body.username })
+        if(user) return res.status(400).json({ success: false, message: "User is already registered" })
+
+        await db.collections.credentials.insertOne({
+            uid: (await db.collections.credentials.countDocuments()) + 1,
+            username: body.username,
+            password: encrypt(password),
+        })
     }) 
 
     return {
---
+++ modules/encrypt.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const key = crypto.createHash('sha256').update(String(process.env.ENCRYPTION_KEY)).digest('base64').substr(0, 32);
+const iv = crypto.randomBytes(16);
+
+module.exports = {
+    encrypt(text) {
+        let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+        let encrypted = cipher.update(text);
+        encrypted = Buffer.concat([encrypted, cipher.final()]);
+        return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+    },
+
+    decrypt(text) {
+        let iv = Buffer.from(text.iv, 'hex');
+        let encryptedText = Buffer.from(text.encryptedData, 'hex');
+        let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+        let decrypted = decipher.update(encryptedText);
+        decrypted = Buffer.concat([decrypted, decipher.final()]);
+        return decrypted.toString();
+    }
+}
+
