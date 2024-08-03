8c6af5e8acf6b12d9a75171fd8f1bbcacf3d368d
Cmb <71194682+29cmb@users.noreply.github.com>
2024-08-03 11:52:17 -0400

    Finish main page

--- views/global/info.js
+++ views/global/info.js
@@ -1,5 +1,5 @@
 async function getUserInformation(){
-    var user = null
+    var user
     await fetch("/api/v1/user/info").then(response => response.json()).then(data => {
         console.log(data)
         user = data.user
