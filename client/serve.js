7d23aba3d5e281d8368a9ef5c208e5839b4dc298
Cmb <71194682+29cmb@users.noreply.github.com>
2024-08-09 15:51:18 -0400

    Adding serve function

---
+++ client/serve.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api', createProxyMiddleware({
+  target: 'http://localhost:3001',
+  changeOrigin: true,
+}));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
