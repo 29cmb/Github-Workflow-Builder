ec9bee2697babedf453677d0fcae28ddbf1d317b
DevCmb <71194682+29cmb@users.noreply.github.com>
2024-08-15 16:48:33 -0400

    better styling + update template

--- client/src/components/Export.jsx
+++ client/src/components/Export.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "../styles/Export.css";
-import defText from "../export/default.yml";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import defText from "../export/default.yml";
 import style from "../export/style"
 
 function Export({ components, dragComponents, componentData, setVisible }) {
+    var exportText = defText
+    componentData.forEach(element => {
+        
+    });
     return (
         <div id="ExportMenu">
             <h1 id="exportText">Export</h1>
@@ -12,9 +16,10 @@ function Export({ components, dragComponents, componentData, setVisible }) {
                 <button id="exit" onClick={() => setVisible(false)}>
                     <img src="/assets/Exit.png" alt="Exit" />
                 </button>
+                <p id="codeNote">Place this code in your<br/><code>.github/workflows</code> folder as a <code>.yml</code></p>
                 <div id="codeblock">
                     <SyntaxHighlighter language="yaml" style={style}>
-                        {defText}
+                        {exportText}
                     </SyntaxHighlighter>
                 </div>
             </div>
